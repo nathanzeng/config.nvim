ff5a35ea506f8fb990b087a1397739442db46b36
Nathan Zeng <nathan.j.zeng@gmail.com>
2025-12-17 10:08:29 -0800

    Don't put stuff in the setup table

--- lua/custom/plugins/diffview.lua
+++ lua/custom/plugins/diffview.lua
@@ -1,8 +1,7 @@
 return {
   'sindrets/diffview.nvim',
   config = function()
-    require('diffview').setup {
-      vim.keymap.set('n', '<leader>git', '<cmd>DiffviewOpen<CR>', { desc = '[git] Diff' }),
-    }
+    require('diffview').setup {}
+    vim.keymap.set('n', '<leader>git', '<cmd>DiffviewOpen<CR>', { desc = '[git] Diff' })
   end,
 }
