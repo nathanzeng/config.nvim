baba87d0295a8f066e5c653e3471e6ca751d8931
Nathan Zeng <nathan.j.zeng@gmail.com>
2026-01-10 20:55:08 -0800

    tweak colors for colorblindness

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -2,29 +2,37 @@ return {
   'EdenEast/nightfox.nvim',
   priority = 1000, -- Make sure to load this before all the other start plugins.
   config = function()
-    require 'nightfox'
+    local palettes = {
+      -- Everything defined under `all` will be applied to every style, can alternatively apply to specific themes
+      all = {
+        -- Each palette defines these colors:
+        --   black, red, green, yellow, blue, magenta, cyan, white, orange, pink
+        --
+        -- These colors have 3 shades: base, bright, and dim
+        --
+        -- Defining just a color defines it's base color
+
+        -- Use a more colorblind friendly blue and magenta/purple
+        blue = '#0072B2',
+        magenta = '#6F6AE1',
+      },
+    }
+
+    local specs = {
+      all = {
+        syntax = {
+          -- Specs allow you to define a value using either a color or template. If the string does
+          -- start with `#` the string will be used as the path of the palette table. Defining just
+          -- a color uses the base version of that color.
+          --NOTE: playing around to try to make the vue files look good
+          keyword = 'yellow',
+          number = 'magenta',
+          const = 'magenta',
+        },
+      },
+    }
+
+    require('nightfox').setup { palettes = palettes, specs = specs }
     vim.cmd.colorscheme 'nightfox'
   end,
 }
-
--- I don't actually like the colorblind settings for nightfox, but this is what I tested
--- return {
---   'EdenEast/nightfox.nvim',
---   priority = 1000, -- Make sure to load this before all the other start plugins.
---   config = function()
---     require('nightfox').setup {
---       options = {
---         colorblind = {
---           enable = true,
---           severity = {
---             protan = 0.3,
---             deutan = 1,
---             tritan = 0,
---           },
---         },
---       },
---     }
---
---     vim.cmd.colorscheme 'nightfox'
---   end,
--- }
