836f582808953a5bed12862976964f511b59e930
Nathan Zeng <nathan.j.zeng@gmail.com>
2026-01-28 19:57:49 -0800

    (telescope): use picker for git status (diff)

--- lua/custom/plugins/telescope.lua
+++ lua/custom/plugins/telescope.lua
@@ -87,10 +87,11 @@ return {
     vim.keymap.set('n', '<leader>fs', builtin.builtin, { desc = '[f]ind [S]elect Telescope' })
     vim.keymap.set('n', '<leader>fw', builtin.grep_string, { desc = '[f]ind current [W]ord' })
     vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = '[f]ind by [G]rep' })
-    vim.keymap.set('n', '<leader>fd', builtin.diagnostics, { desc = '[f]ind [D]iagnostics' })
+    vim.keymap.set('n', '<leader>fe', builtin.diagnostics, { desc = '[f]ind diagnostic [e]rrors' })
     vim.keymap.set('n', '<leader>fr', builtin.resume, { desc = '[f]ind [R]esume' })
     vim.keymap.set('n', '<leader>f.', builtin.oldfiles, { desc = '[f]ind Recent Files ("." for repeat)' })
     vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+    vim.keymap.set('n', '<leader>fd', builtin.git_status, { desc = '[f]ind git [d]iff' })
 
     -- Slightly advanced example of overriding default behavior and theme
     vim.keymap.set('n', '<leader>/', function()
