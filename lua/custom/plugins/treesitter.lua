8e7e49f8bf9d290a33a7124e922fe04aa0d64dee
Nathan Zeng <nathan.j.zeng@gmail.com>
2026-02-05 21:15:49 -0800

    (treesitter): add a bunch of languages to install

--- lua/custom/plugins/treesitter.lua
+++ lua/custom/plugins/treesitter.lua
@@ -1,9 +1,36 @@
 return { -- Highlight, edit, and navigate code
   'nvim-treesitter/nvim-treesitter',
+  lazy = false,
+  build = ':TSUpdate',
   config = function()
-    local filetypes =
-      { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' }
+    local filetypes = {
+      'bash',
+      'c',
+      'diff',
+      'html',
+      'lua',
+      'luadoc',
+      'markdown',
+      'markdown_inline',
+      'query',
+      'vim',
+      'vimdoc',
+      'vue',
+      'typescript',
+      'javascript',
+      'tsx',
+      'jsx',
+      'css',
+      'php',
+      'json',
+      'gitcommit',
+      'gitignore',
+      'git_rebase',
+      'yaml',
+      'graphql',
+    }
     require('nvim-treesitter').install(filetypes)
+
     vim.api.nvim_create_autocmd('FileType', {
       pattern = filetypes,
       callback = function()
