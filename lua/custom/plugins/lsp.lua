53724388b249316bf5f17cc71b326bb7b28d7d8a
Nathan Zeng <nathan.j.zeng@gmail.com>
2026-03-05 23:27:32 -0800

    (lsp): remove dead code

--- lua/custom/plugins/lsp.lua
+++ lua/custom/plugins/lsp.lua
@@ -306,24 +306,6 @@ return {
       },
     }
 
-    -- Ensure the servers and tools above are installed
-    --
-    -- To check the current status of installed tools and/or manually install
-    -- other tools, you can run
-    --    :Mason
-    --
-    -- You can press `g?` for help in this menu.
-    --
-    -- `mason` had to be setup earlier: to configure its options see the
-    -- `dependencies` table for `nvim-lspconfig` above.
-    --
-    -- You can add other tools here that you want Mason to install
-    -- for you, so that they are available from within Neovim.
-    local ensure_installed = vim.tbl_keys(servers or {})
-    vim.list_extend(ensure_installed, {
-      'stylua', -- Used to format Lua code
-    })
-
     -- Now setup those configurations
     for name, server in pairs(servers) do
       vim.lsp.config(name, server)
