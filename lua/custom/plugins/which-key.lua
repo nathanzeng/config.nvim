d735b0a611ee9dffe8341e166b632ac815015162
Nathan Zeng <nathan.j.zeng@gmail.com>
2026-03-06 23:10:58 -0800

    (which-key): clean up unnecessary code

--- lua/custom/plugins/which-key.lua
+++ lua/custom/plugins/which-key.lua
@@ -18,15 +18,6 @@ return {
     -- delay between pressing a key and opening which-key (milliseconds)
     -- this setting is independent of vim.o.timeoutlen
     delay = 400,
-    icons = {
-      -- set icon mappings to true if you have a Nerd Font
-      mappings = vim.g.have_nerd_font,
-      -- If you are using a Nerd Font: set icons.keys to an empty table which will use the
-      -- default which-key.nvim defined Nerd Font icons, otherwise define a string table
-      keys = {},
-    },
-
-    -- TODO: Git [H]unk from gitsigns isn't enabled by default, submit issue/PR?
     -- Document existing key chains
     spec = {
       { '<leader>f', group = '[f]ind' },
