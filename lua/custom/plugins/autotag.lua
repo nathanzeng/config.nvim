6b671c77365b2956a4934f01e928257d47b17ec4
Nathan Zeng <nathan.j.zeng@gmail.com>
2026-03-18 22:08:32 -0700

    (autotag): even lazier

--- lua/custom/plugins/autotag.lua
+++ lua/custom/plugins/autotag.lua
@@ -1,7 +1,7 @@
 -- Autotags for html style tags
 return {
   'windwp/nvim-ts-autotag',
-  event = { 'BufReadPre', 'BufNewFile' },
+  event = 'InsertEnter',
   config = function()
     require('nvim-ts-autotag').setup {
       opts = {
