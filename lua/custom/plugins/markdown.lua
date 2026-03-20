a8f4214e86c995d8b2a981b5c4cea0b7bd0acf82
Nathan Zeng <nathan.j.zeng@gmail.com>
2026-03-20 08:48:28 -0700

    (markdown): render markdown plugin

---
+++ lua/custom/plugins/markdown.lua
@@ -0,0 +1,6 @@
+return {
+  'MeanderingProgrammer/render-markdown.nvim',
+  version = '*',
+  dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' },
+  ft = { 'markdown', 'codecompanion' },
+}
