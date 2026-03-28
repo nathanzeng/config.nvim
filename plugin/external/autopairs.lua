7e813196067a0612cdf1f38048bdaa0541a40981
Nathan Zeng <nathan.j.zeng@gmail.com>
2026-03-28 11:59:04 -0700

    autopairs

--- plugin/external/autopairs.lua
+++ plugin/external/autopairs.lua
@@ -1,8 +1,2 @@
--- autopairs
--- https://github.com/windwp/nvim-autopairs
-
-return {
-  'windwp/nvim-autopairs',
-  event = 'InsertEnter',
-  opts = {},
-}
+vim.pack.add({ 'https://github.com/windwp/nvim-autopairs' })
+require('nvim-autopairs').setup()
