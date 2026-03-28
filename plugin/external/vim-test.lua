2274238cf4910fccd03c5f21b5bae890357bf54e
Nathan Zeng <nathan.j.zeng@gmail.com>
2026-03-28 15:08:25 -0700

    vim-test

--- plugin/external/vim-test.lua
+++ plugin/external/vim-test.lua
@@ -1,12 +1,9 @@
-return {
-  'vim-test/vim-test',
-  config = function()
-    -- These were written as vimscript in the README and this is what LLM gave me
-    vim.g['test#strategy'] = 'neovim'
-    vim.g['test#neovim#term_position'] = 'vert'
+vim.pack.add({ 'https://github.com/vim-test/vim-test' })
 
-    vim.keymap.set('n', '<leader>t', '<CMD>TestNearest<CR>', { desc = '[t]est nearest' })
-    vim.keymap.set('n', '<leader>td', '<CMD>TestNearest XDEBUG_SESSION=1<CR>', { desc = '[t]est [d]ebug nearest' })
-    vim.keymap.set('n', '<leader>tf', '<CMD>TestFile<CR>', { desc = '[t]est [f]ile' })
-  end,
-}
+-- These were written as vimscript in the README and this is what LLM gave me
+vim.g['test#strategy'] = 'neovim'
+vim.g['test#neovim#term_position'] = 'vert'
+
+vim.keymap.set('n', '<leader>t', '<CMD>TestNearest<CR>', { desc = '[t]est nearest' })
+vim.keymap.set('n', '<leader>td', '<CMD>TestNearest XDEBUG_SESSION=1<CR>', { desc = '[t]est [d]ebug nearest' })
+vim.keymap.set('n', '<leader>tf', '<CMD>TestFile<CR>', { desc = '[t]est [f]ile' })
