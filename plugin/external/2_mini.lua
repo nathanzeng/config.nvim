d84d1abebce62b282848f8c1271f584167d8c793
Nathan Zeng <nathan.j.zeng@gmail.com>
2026-05-08 21:37:08 -0700

    mini: clue quickstart

--- plugin/external/2_mini.lua
+++ plugin/external/2_mini.lua
@@ -39,10 +39,49 @@ vim.schedule(function()
     },
   })
 
-  -- TODO: for comparison testing, remove
-  -- require('mini.indentscope').setup({
-  --   draw = { delay = 0, animation = require('mini.indentscope').gen_animation.none() },
-  -- })
+  local miniclue = require('mini.clue')
+  miniclue.setup({
+    delay = 600,
+    triggers = {
+      -- Leader triggers
+      { mode = { 'n', 'x' }, keys = '<Leader>' },
+
+      -- `[` and `]` keys
+      { mode = 'n', keys = '[' },
+      { mode = 'n', keys = ']' },
+
+      -- Built-in completion
+      { mode = 'i', keys = '<C-x>' },
+
+      -- `g` key
+      { mode = { 'n', 'x' }, keys = 'g' },
+
+      -- Marks
+      { mode = { 'n', 'x' }, keys = "'" },
+      { mode = { 'n', 'x' }, keys = '`' },
+
+      -- Registers
+      { mode = { 'n', 'x' }, keys = '"' },
+      { mode = { 'i', 'c' }, keys = '<C-r>' },
+
+      -- Window commands
+      { mode = 'n', keys = '<C-w>' },
+
+      -- `z` key
+      { mode = { 'n', 'x' }, keys = 'z' },
+    },
+
+    clues = {
+      -- Enhance this by adding descriptions for <Leader> mapping groups
+      miniclue.gen_clues.square_brackets(),
+      miniclue.gen_clues.builtin_completion(),
+      miniclue.gen_clues.g(),
+      miniclue.gen_clues.marks(),
+      miniclue.gen_clues.registers(),
+      miniclue.gen_clues.windows(),
+      miniclue.gen_clues.z(),
+    },
+  })
 end)
 
 -- Icons and mock the nvim-tree one
