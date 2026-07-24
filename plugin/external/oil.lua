67844d922b9086d922151cd17cd04645bb2d8504
Nathan Zeng <nathan.j.zeng@gmail.com>
2026-07-24 13:32:33 -0700

    oil: keymap for opening cwd

--- plugin/external/oil.lua
+++ plugin/external/oil.lua
@@ -38,4 +38,6 @@ require('oil').setup({
     ['go'] = { 'actions.refresh' },
   },
 })
+
 vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
+vim.keymap.set('n', '_', '<CMD>e .<CR>', { desc = 'Open CWD' })
