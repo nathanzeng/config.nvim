29ccdbf7872bec2ed5e745a14f36331b37bc54b7
Nathan Zeng <nathan.j.zeng@gmail.com>
2026-07-24 13:50:54 -0700

    keymaps: <leader>w peforms :update instead

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,4 +1,4 @@
-vim.keymap.set('n', '<leader>w', '<cmd>w<CR>', { desc = '[w]rite' })
+vim.keymap.set('n', '<leader>w', '<cmd>update<CR>', { desc = '[w]rite' })
 vim.keymap.set('n', '<leader>x', '<cmd>xa!<CR>', { desc = 'Quit [x]a!' })
 vim.keymap.set('t', '<Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
