c8bff710dd90b75ab9ae40b3e209f7f743af142b
Nathan Zeng <nathan.j.zeng@gmail.com>
2026-08-03 21:40:44 -0700

    keymaps: don't override <C-u> in insert mode
    The capitalization mapping is being mapped to ctrl-l instead

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -44,8 +44,7 @@ vim.keymap.set('x', 'P', 'p')
 -- Paste from yank register
 vim.keymap.set({ 'n', 'x' }, '<leader>op', '"0p', { desc = 'Paste from yank register "0' })
 
--- Note: native functionality is to delete everything on the line preceding the cursor
-vim.keymap.set('i', '<C-u>', '<Esc>gUiw`]a', { desc = '[u]pper case the word before cursor' })
+vim.keymap.set('i', '<C-l>', '<Esc>gUiw`]a', { desc = 'Caps [l]ock the word before cursor' })
 
 -- Restart neovim
 vim.keymap.set('n', '<leader>n', '<cmd>restart<CR>', { desc = '[n]eovim restart' })
