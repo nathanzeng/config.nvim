e8d4096ba6434f9f7bd51ee0ddcde26d904e266c
Nathan Zeng <nathan.j.zeng@gmail.com>
2026-08-10 21:04:05 -0700

    dir: allow opening with <C-y>

--- plugin/dir.lua
+++ plugin/dir.lua
@@ -54,6 +54,9 @@ api.nvim_create_autocmd('FileType', {
       vim.cmd.edit('#')
     end, { desc = 'Close dir buffer', buf = 0 })
 
+    vim.keymap.set('n', '<C-y>', '<Plug>(nvim-dir-open)', { desc = 'Open Entry', buf = 0 })
+    vim.keymap.set('n', '<CR>', '<Plug>(nvim-dir-open)', { desc = 'Open Entry', buf = 0 })
+
     -- Add entry
     vim.keymap.set('n', 'o', function()
       vim.ui.input({ prompt = 'Add: ' }, function(input)
