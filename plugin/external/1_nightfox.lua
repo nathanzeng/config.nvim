2a561c0195b824b8e15d6803880690cac20cfa75
Nathan Zeng <nathan.j.zeng@gmail.com>
2026-08-10 23:31:40 -0700

    guicursor: bar in terminal mode

--- plugin/external/1_nightfox.lua
+++ plugin/external/1_nightfox.lua
@@ -36,9 +36,9 @@ local specs = {
 require('nightfox').setup({ specs = specs })
 vim.cmd.colorscheme('nordfox')
 
--- Cursor color for visual, and never blinks
+-- Cursor color for visual, and bar cursor for terminal
 vim.api.nvim_set_hl(0, 'VisualCursor', { bg = '#8cafd2' })
-vim.opt.guicursor:append('v:VisualCursor,a:blinkon0')
+vim.opt.guicursor:append('v:VisualCursor,t:ver25')
 
 -- Showkeys
 -- vim.pack.add({ 'https://github.com/NvChad/showkeys' })
