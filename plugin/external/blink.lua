190d4fbc9b0c84138b4aa801d105059fc0d3621b
Nathan Zeng <nathan.j.zeng@gmail.com>
2026-08-30 20:28:21 -0700

    blink: clean up comments and remove ctrl-e conflict

--- plugin/external/blink.lua
+++ plugin/external/blink.lua
@@ -3,35 +3,15 @@ vim.pack.add({
     src = 'https://github.com/saghen/blink.cmp',
     version = 'v1.10.2',
   },
+  'https://github.com/saghen/blink.indent',
 })
 
 require('blink.cmp').setup({
   keymap = {
-    -- 'default' (recommended) for mappings similar to built-in completions
-    --   <c-y> to accept ([y]es) the completion.
-    --    This will auto-import if your LSP supports it.
-    --    This will expand snippets if the LSP sent a snippet.
-    -- 'super-tab' for tab to accept
-    -- 'enter' for enter to accept
-    -- 'none' for no mappings
-    --
-    -- For an understanding of why the 'default' preset is recommended,
-    -- you will need to read `:help ins-completion`
-    --
-    -- No, but seriously. Please read `:help ins-completion`, it is really good!
-    --
-    -- All presets have the following mappings:
-    -- <tab>/<s-tab>: move to right/left of your snippet expansion
-    -- <c-space>: Open menu or open docs if already open
-    -- <c-n>/<c-p> or <up>/<down>: Select next/previous item
-    -- <c-e>: Hide menu
-    -- <c-k>: Toggle signature help
-    --
-    -- See :h blink-cmp-config-keymap for defining your own keymap
     preset = 'default',
-
-    -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
-    --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
+    -- I have ctrl-e mapped to jumping to the end of line (bash style)
+    ['<C-e>'] = false,
+    ['<C-h>'] = { 'hide', 'fallback' },
   },
 
   completion = {
@@ -39,7 +19,6 @@ require('blink.cmp').setup({
       selection = {
         -- When `true`, inserts the completion item automatically when selecting it
         auto_insert = false,
-        -- auto_insert = function(ctx) return vim.bo.filetype ~= 'markdown' end
       },
     },
     menu = {
@@ -55,8 +34,6 @@ require('blink.cmp').setup({
   signature = { enabled = true },
 })
 
--- Indent lines and text object
-vim.pack.add({ 'https://github.com/saghen/blink.indent' })
 require('blink.indent').setup({
   -- No need to see all the indent lines
   static = { enabled = false },
