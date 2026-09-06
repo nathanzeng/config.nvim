b54b35ffdb51c5f829c9bf9ae27127273dc1a23d
Nathan Zeng <nathan.j.zeng@gmail.com>
2026-09-05 18:09:39 -0700

    mini: remove patch for multicursor

--- plugin/external/2_mini.lua
+++ plugin/external/2_mini.lua
@@ -36,9 +36,6 @@ vim.schedule(function()
     },
   })
 
-  -- TODO: remove this once mini addresses
-  vim.keymap.set('n', 'Q', 'Q')
-
   local miniclue = require('mini.clue')
   miniclue.setup({
     window = {
